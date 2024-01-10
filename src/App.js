826bfee95abdb1872872e5b9b477e5f5e38a5f1b
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2024-01-11 01:33:17 +0500

    interceptors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
+import { APIuser } from './Services/APIS';
+import axios from 'axios';
 
+//interceptor config
+const successReq = (response) => {
+  document.body.classList.add('loading-indicator');
+  // const token = localStorage.getItem("token")
+  // response.headers.Authorization = token;
+  // console.log("SUCCESS response", response)
+  return response;
+}
+const success = (response) => {
+  document.body.classList.remove('loading-indicator');
+  // const token = localStorage.getItem("token")
+  // response.headers.Authorization = token;
+  // console.log("SUCCESS response", response)
+  return response;
+}
+const failed = (error) => {
+  document.body.classList.remove('loading-indicator');
+  // console.log("error response" + error);
+  return Promise.reject(error);
+}
+// APIuser.interceptors.request.use(successReq, failed);
+// axios.interceptors.request.use(successReq, failed);
+// axios.interceptors.response.use(success, failed)
+// APIuser.interceptors.response.use(success, failed)
 function App() {
   useEffect(()=>{
     // throw new Error("Hello error")
